e2fa9915a1ddf1e8411e3192e8e81b90984ea940
Mikey Donohue <mwdonohue1234@gmail.com>
2022-01-16 16:10:48 -0500

    Started refactoring, switched to Gin

--- main.go
+++ main.go
@@ -13,6 +13,7 @@ import (
 
 	"weather-route/utils"
 
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"googlemaps.github.io/maps"
 )
@@ -35,36 +36,56 @@ type RoutePoints struct {
 var config Configuration
 var mapClient *maps.Client
 
-func getDirections(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Set("Access-Control-Allow-Origin", "*")
-	rw.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	decoder := json.NewDecoder(r.Body)
-	var routePoints RoutePoints
+type DirectionsRetriever interface {
+	Retrieve(routePoints RoutePoints) (routes interface{}, err error)
+}
 
-	err := decoder.Decode(&routePoints)
+type DirectionsClient struct {
+	mapClient *maps.Client
+}
+
+func (client DirectionsClient) Retrieve(routePoints RoutePoints) (routes interface{}, err error) {
+	computedRoutes, _, err := client.mapClient.Directions(context.Background(), &maps.DirectionsRequest{Origin: routePoints.Origin, Destination: routePoints.Destination, Mode: maps.TravelModeDriving})
+	if err != nil {
+		return nil, err
+	}
+	return computedRoutes, nil
+}
+
+func getDirections(c *gin.Context, directionsRetriever DirectionsRetriever) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "Content-Type")
+
+	var routePoints RoutePoints
+	err := c.ShouldBindJSON(&routePoints)
 	if err != nil {
 		log.Printf("Unable to decode directions: %s\n", err.Error())
-		http.Error(rw, "Unable to decode directions", http.StatusBadRequest)
+
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Unable to decode directions"})
 		return
 	}
 
-	route, _, _ := mapClient.Directions(context.Background(), &maps.DirectionsRequest{
-		Origin:      routePoints.Origin,
-		Destination: routePoints.Destination,
-		Mode:        maps.TravelModeDriving,
-	})
-	json.NewEncoder(rw).Encode(map[string]interface{}{"routes": route, "travelMode": "DRIVING"})
+	route, directionsErr := directionsRetriever.Retrieve(routePoints)
+
+	if directionsErr != nil {
+		log.Printf("Unable to get directions: %s\n", directionsErr.Error())
+
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Unable to get directions"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"routes": route, "travelMode": "DRIVING"})
 }
 
-func getAutoCompleteSuggestions(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Set("Access-Control-Allow-Origin", "*")
-	rw.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	decoder := json.NewDecoder(r.Body)
+func getAutoCompleteSuggestions(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "Content-Type")
 	var input PlaceAutocompleteInput
-	err := decoder.Decode(&input)
+	err := c.ShouldBindJSON(&input)
+
 	if err != nil {
 		log.Printf("Unable to decode autocomplete suggestions: %s\n", err.Error())
-		http.Error(rw, "Unable to decode autocomplete suggestions", http.StatusBadRequest)
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Unable to decode autocomplete suggestions"})
 		return
 	}
 
@@ -77,7 +98,7 @@ func getAutoCompleteSuggestions(rw http.ResponseWriter, r *http.Request) {
 
 	if autocompleteError != nil {
 		log.Printf("Unable to use autocomplete client: %s\n", autocompleteError.Error())
-		http.Error(rw, "Unable to use autocomplete client", 500)
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Unable to use autocomplete client"})
 		return
 	}
 
@@ -87,94 +108,90 @@ func getAutoCompleteSuggestions(rw http.ResponseWriter, r *http.Request) {
 		resp = append(resp, v.Description)
 	}
 	if autoCompleteResponse.Predictions != nil {
-		json.NewEncoder(rw).Encode(resp)
+		// json.NewEncoder(rw).Encode(resp)
+		c.JSON(http.StatusOK, resp)
 	} else {
-		json.NewEncoder(rw).Encode([]string{})
+		c.JSON(http.StatusNoContent, []string{})
 	}
 }
 
-func getWeather(rw http.ResponseWriter, r *http.Request) {
-	rw.Header().Set("Access-Control-Allow-Origin", "*")
-	rw.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-	switch r.Method {
-	case "POST":
-		decoder := json.NewDecoder(r.Body)
-		var routes []maps.Route
-		err := decoder.Decode(&routes)
-		if err != nil {
-			log.Printf("Unable to decode routes for weather: %s\n", err.Error())
-			http.Error(rw, "Unable to decode routes", 400)
-			return
-		}
-
-		coords, getCoordsError := utils.GetCoords(routes)
-		if getCoordsError != nil {
-			log.Printf("Unable to retrieve coordinates for the given route")
-			http.Error(rw, "Unable to retrieve coordinates for the given route: %s\n", http.StatusInternalServerError)
-			return
-		}
-		coordsEveryFiveMiles := utils.GetCoordsEveryNMeters(coords, 8046.72)
-		weatherApiWG := sync.WaitGroup{}
-		weatherCoords := make([]utils.CoordinateWeather, 0)
-		for _, coordTime := range coordsEveryFiveMiles {
-			// Get weather data for coordinate
-			weatherApiWG.Add(1)
-			go func(coordTime utils.CoordTime) {
-				weatherForCoord, weatherAPIError := http.Get("https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" +
-					fmt.Sprint(coordTime.Coord.Lat) + "&lon=" +
-					fmt.Sprint(coordTime.Coord.Lng) + "&exclude=current,minutely,daily,alerts&appid=" +
-					config.OpenWeatherAPIKey)
-
-				// Make sure the response returns 200
-				if weatherForCoord.StatusCode != 200 {
-					log.Printf("Unable to retrieve weather data for coordinate: internal API error with code %d\n", weatherForCoord.StatusCode)
-					http.Error(rw, "Unable to retrieve weather data for coordinate: internal API error", http.StatusServiceUnavailable)
-					return
-				}
-
-				if weatherAPIError != nil {
-					log.Printf("Unable to retrieve weather data for coordinate: %s\n", weatherAPIError.Error())
-					http.Error(rw, "Unable to retrieve weather data for a coordinate", http.StatusInternalServerError)
-					weatherApiWG.Done()
-					return
-				}
-				weatherApiWG.Done()
-				body, parseBodyError := ioutil.ReadAll(weatherForCoord.Body)
-				if parseBodyError != nil {
-					log.Printf("Unable to parse the body of the weather for coordinate: %s\n", parseBodyError.Error())
-					http.Error(rw, "Unable to parse the body of hte weather for coordinate", http.StatusInternalServerError)
-					return
-				}
-				var weather map[string]interface{}
-				unmarshallingError := json.Unmarshal(body, &weather)
-				if unmarshallingError != nil {
-					log.Printf("Unable to unmarshall the weather's JSON data for coordinate: %s\n", unmarshallingError.Error())
-					http.Error(rw, "Unable to unmarshall the weather's JSON data for coordinate", http.StatusBadRequest)
-				}
-
-				// Find hourly entry associated with hour in coordTime
-				for _, hourlyEntry := range weather["hourly"].([]interface{}) {
-					unixTime := time.Unix(int64(hourlyEntry.(map[string]interface{})["dt"].(float64)), 0)
-					unixTime = unixTime.UTC()
-					if unixTime.Hour() == coordTime.TimeAtCoord.Hour() {
-						weatherCoords = append(weatherCoords,
-							utils.CoordinateWeather{
-								Coord: coordTime.Coord,
-								WeatherData: utils.Weather{
-									Temperature:   hourlyEntry.(map[string]interface{})["temp"].(float64),
-									Precipitation: hourlyEntry.(map[string]interface{})["pop"].(float64),
-									Icon:          hourlyEntry.(map[string]interface{})["weather"].([]interface{})[0].(map[string]interface{})["icon"].(string),
-								},
-								Time: coordTime.TimeAtCoord,
-							})
-						break
-					}
-				}
-			}(coordTime)
-		}
-		weatherApiWG.Wait()
-		json.NewEncoder(rw).Encode(weatherCoords)
+func getWeather(c *gin.Context) {
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "Content-Type")
+	var routes []maps.Route
+	err := c.ShouldBindJSON(&routes)
+	if err != nil {
+		log.Printf("Unable to decode routes for weather: %s\n", err.Error())
+		c.JSON(http.StatusBadRequest, gin.H{"message": "Unable to decode routes"})
+		return
 	}
+
+	coords, getCoordsError := utils.GetCoords(routes)
+	if getCoordsError != nil {
+		log.Printf("Unable to retrieve coordinates for the given route")
+		c.JSON(http.StatusInternalServerError, gin.H{"message": "Unabel to retrieve coordinates for the given route"})
+		return
+	}
+	coordsEveryFiveMiles := utils.GetCoordsEveryNMeters(coords, 8046.72)
+	weatherApiWG := sync.WaitGroup{}
+	weatherCoords := make([]utils.CoordinateWeather, 0)
+	for _, coordTime := range coordsEveryFiveMiles {
+		// Get weather data for coordinate
+		weatherApiWG.Add(1)
+		go func(coordTime utils.CoordTime) {
+			weatherForCoord, weatherAPIError := http.Get("https://api.openweathermap.org/data/2.5/onecall?units=imperial&lat=" +
+				fmt.Sprint(coordTime.Coord.Lat) + "&lon=" +
+				fmt.Sprint(coordTime.Coord.Lng) + "&exclude=current,minutely,daily,alerts&appid=" +
+				config.OpenWeatherAPIKey)
+
+			// Make sure the response returns 200
+			if weatherForCoord.StatusCode != 200 {
+				log.Printf("Unable to retrieve weather data for coordinate: internal API error with code %d\n", weatherForCoord.StatusCode)
+				c.JSON(http.StatusServiceUnavailable, gin.H{"message": "Unable to retrieve weather data for coordiante: internal API error"})
+				return
+			}
+
+			if weatherAPIError != nil {
+				log.Printf("Unable to retrieve weather data for coordinate: %s\n", weatherAPIError.Error())
+				weatherApiWG.Done()
+				return
+			}
+			weatherApiWG.Done()
+			body, parseBodyError := ioutil.ReadAll(weatherForCoord.Body)
+			if parseBodyError != nil {
+				log.Printf("Unable to parse the body of the weather for coordinate: %s\n", parseBodyError.Error())
+				c.JSON(http.StatusInternalServerError, gin.H{"message": "Unable to parse the body of the weather for coordinate"})
+				return
+			}
+			var weather map[string]interface{}
+			unmarshallingError := json.Unmarshal(body, &weather)
+			if unmarshallingError != nil {
+				log.Printf("Unable to unmarshall the weather's JSON data for coordinate: %s\n", unmarshallingError.Error())
+				c.JSON(http.StatusBadRequest, gin.H{"message": "Unable to marshall the weather's JSON data for coordinate"})
+			}
+
+			// Find hourly entry associated with hour in coordTime
+			for _, hourlyEntry := range weather["hourly"].([]interface{}) {
+				unixTime := time.Unix(int64(hourlyEntry.(map[string]interface{})["dt"].(float64)), 0)
+				unixTime = unixTime.UTC()
+				if unixTime.Hour() == coordTime.TimeAtCoord.Hour() {
+					weatherCoords = append(weatherCoords,
+						utils.CoordinateWeather{
+							Coord: coordTime.Coord,
+							WeatherData: utils.Weather{
+								Temperature:   hourlyEntry.(map[string]interface{})["temp"].(float64),
+								Precipitation: hourlyEntry.(map[string]interface{})["pop"].(float64),
+								Icon:          hourlyEntry.(map[string]interface{})["weather"].([]interface{})[0].(map[string]interface{})["icon"].(string),
+							},
+							Time: coordTime.TimeAtCoord,
+						})
+					break
+				}
+			}
+		}(coordTime)
+	}
+	weatherApiWG.Wait()
+	c.JSON(http.StatusOK, weatherCoords)
 }
 
 func init() {
@@ -205,11 +222,19 @@ func main() {
 	if os.Getenv("PORT") == "" {
 		port = ":5000"
 	}
-	http.HandleFunc("/weather", getWeather)
-	http.HandleFunc("/autoCompleteSuggestions", getAutoCompleteSuggestions)
-	http.HandleFunc("/directions", getDirections)
-	serve := http.FileServer(http.Dir("./static"))
-	http.Handle("/", serve)
+
+	serv := gin.Default()
+	serv.POST("/weather", getWeather)
+	serv.POST("/autoCompleteSuggestions", getAutoCompleteSuggestions)
+
+	directionsClient := &DirectionsClient{mapClient: mapClient}
+	serv.POST("/directions", func(c *gin.Context) {
+		getDirections(c, directionsClient)
+	})
+
+	serv.StaticFS("/", http.Dir("./static"))
+
+	serv.Run(port)
 	log.Println("Listening on port: " + port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
